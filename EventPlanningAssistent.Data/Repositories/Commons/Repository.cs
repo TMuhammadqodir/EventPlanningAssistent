bf2918f5bb94007d763e23a117a0ae65b3977e33
TMuhammadqodir <muhammadqodirturdiyev65@gmail.com>
2023-08-10 19:19:42 +0500

    ReadyMetodsInRepository

--- EventPlanningAssistent.Data/Repositories/Commons/Repository.cs
+++ EventPlanningAssistent.Data/Repositories/Commons/Repository.cs
@@ -1,32 +1,40 @@
-﻿using EventPlanningAssistent.Data.IRepositories.Commons;
+﻿using EventPlanningAssistent.Data.DbContexts;
+using EventPlanningAssistent.Data.IRepositories.Commons;
 using EventPlanningAssistent.Domain.Commons;
+using Microsoft.EntityFrameworkCore;
 
 namespace EventPlanningAssistent.Data.Repositories.Commons;
 
 public class Repository<T> : IRepository<T> where T : Auditable
 {
-    public Task CreateAsync(T entity)
+    private readonly AppDbContext appDbContext;
+    private readonly DbSet<T> dbSet;
+
+    public Repository(AppDbContext appDbContext)
     {
-        throw new NotImplementedException();
+        this.appDbContext = appDbContext;
+
+        dbSet = this.appDbContext.Set<T>();
+    }
+
+    public async Task CreateAsync(T entity)
+    {
+        await dbSet.AddAsync(entity);
     }
 
     public void Delete(T entity)
     {
-        throw new NotImplementedException();
-    }
-
-    public IQueryable<T> GetAll()
-    {
-        throw new NotImplementedException();
-    }
-
-    public Task<T> GetById(long id)
-    {
-        throw new NotImplementedException();
+        dbSet.Remove(entity);
     }
 
     public void Update(T entity)
     {
-        throw new NotImplementedException();
+        dbSet.Entry(entity).State = EntityState.Modified;
     }
+    public IQueryable<T> GetAll()
+        => dbSet.AsQueryable();
+
+    public async Task<T> GetById(long id)
+        => await dbSet.FirstOrDefaultAsync(x => x.Id == id);
+
 }
