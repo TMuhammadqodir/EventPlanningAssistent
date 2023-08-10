0c6fff8e2240fb6b0e7ad7b32ca4422b56b4fbb2
TMuhammadqodir <muhammadqodirturdiyev65@gmail.com>
2023-08-10 17:55:43 +0500

    AddProportiesToVentor

--- EventPlanningAssistent.Domain/Entities/Ventors/VentorEntity.cs
+++ EventPlanningAssistent.Domain/Entities/Ventors/VentorEntity.cs
@@ -1,5 +1,14 @@
-﻿namespace EventPlanningAssistent.Domain.Entities.Ventors;
+﻿using EventPlanningAssistent.Domain.Commons;
+using EventPlanningAssistent.Domain.Entities.Contracts;
+using System.ComponentModel.DataAnnotations.Schema;
 
-public class VentorEntity
+namespace EventPlanningAssistent.Domain.Entities.Ventors;
+
+[Table("Ventors")]
+public class VentorEntity : Auditable
 {
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string TelNumber { get; set; }
+    public ICollection<ContractEntity> contracts { get; set; }
 }
