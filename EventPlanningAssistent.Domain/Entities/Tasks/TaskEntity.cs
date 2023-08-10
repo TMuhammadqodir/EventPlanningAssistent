9e617deabbe601cbee42b7d6bd6124618b1c38ac
TMuhammadqodir <muhammadqodirturdiyev65@gmail.com>
2023-08-10 17:52:59 +0500

    AddProportiesToTasks

--- EventPlanningAssistent.Domain/Entities/Tasks/TaskEntity.cs
+++ EventPlanningAssistent.Domain/Entities/Tasks/TaskEntity.cs
@@ -1,5 +1,18 @@
-﻿namespace EventPlanningAssistent.Domain.Entities.Tasks;
+﻿using EventPlanningAssistent.Domain.Commons;
+using EventPlanningAssistent.Domain.Entities.Attendees;
+using EventPlanningAssistent.Domain.Entities.Events;
+using System.ComponentModel.DataAnnotations.Schema;
 
-public class TaskEntity 
+namespace EventPlanningAssistent.Domain.Entities.Tasks;
+
+[Table("Tasks")]
+public class TaskEntity : Auditable
 {
+    public string Name { get; set; }
+    public DateTime StartTime { get; set; }
+    public DateTime EndTime { get; set; }
+    public string Description { get; set; }
+    public long EventId { get; set; }
+    public EventEntity Event { get; set; }
+    public ICollection<AttendeeEntity> attendees { get; set; }
 }
