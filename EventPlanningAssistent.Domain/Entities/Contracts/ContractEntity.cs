e00c8c0a39efa84282cb62c2ede722291900adcc
TMuhammadqodir <muhammadqodirturdiyev65@gmail.com>
2023-08-10 17:36:07 +0500

    AddPropertiesToContract

--- EventPlanningAssistent.Domain/Entities/Contracts/ContractEntity.cs
+++ EventPlanningAssistent.Domain/Entities/Contracts/ContractEntity.cs
@@ -1,5 +1,12 @@
-﻿namespace EventPlanningAssistent.Domain.Entities.Contracts;
+﻿using EventPlanningAssistent.Domain.Entities.Ventors;
+using System.ComponentModel.DataAnnotations.Schema;
 
+namespace EventPlanningAssistent.Domain.Entities.Contracts;
+
+[Table("Contracts")]
 public class ContractEntity
 {
+    public string ContractDeatails { get; set; }
+    public long VentorId { get; set; }
+    public VentorEntity Ventor { get; set; }
 }
