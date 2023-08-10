63b9e8a0f46dc9292c567a5026b3a335a8a80823
TMuhammadqodir <muhammadqodirturdiyev65@gmail.com>
2023-08-11 00:17:48 +0500

    DoneEventServiceMethods

--- EventPlanningAssistent.Service/Services/EventService.cs
+++ EventPlanningAssistent.Service/Services/EventService.cs
@@ -42,21 +42,90 @@ public class EventService : IEventService
 
     public async Task<Responce<bool>> DeleteAsync(long id)
     {
-        EventEntity eventEntity = await unitOfWork.events.GetById(id);
+        EventEntity existEvent = await unitOfWork.events.GetByIdAsync(id);
+
+        if (existEvent is null)
+            return new Responce<bool>
+            {
+                StatusCode = 404,
+                Message = "This event was not found",
+                Result = false
+            };
+
+        unitOfWork.events.Delete(existEvent);
+        await unitOfWork.SaveAsync();
+
+        return new Responce<bool>
+        {
+            StatusCode = 200,
+            Message = "Success",
+            Result = true
+        };
     }
 
-    public Task<Responce<IEnumerable<EventResultDTO>>> GetAllAysnc()
+    public async Task<Responce<EventResultDTO>> UpdateAsync(EventUpdateDTO dto)
     {
-        throw new NotImplementedException();
+        EventEntity existEvent = await unitOfWork.events.GetByIdAsync(dto.Id);
+
+        if (existEvent is null)
+            return new Responce<EventResultDTO>
+            {
+                StatusCode = 404,
+                Message = "This event was not found",
+            };
+
+        var eventUpdate = mapper.Map(dto, existEvent);
+        
+        unitOfWork.events.Update(eventUpdate);
+        await unitOfWork.SaveAsync();
+
+        var resultEvent = mapper.Map<EventResultDTO>(eventUpdate);
+
+        return new Responce<EventResultDTO>
+        {
+            StatusCode = 200,
+            Message = "Success",
+            Result = resultEvent
+        };
     }
 
-    public Task<Responce<EventResultDTO>> GetByIdAsync(long id)
+    public async Task<Responce<IEnumerable<EventResultDTO>>> GetAllAysnc()
     {
-        throw new NotImplementedException();
+        var events = unitOfWork.events.GetAll();
+
+        var resultevents = new List<EventResultDTO>();
+
+        foreach(EventEntity entity in events)
+        {
+            resultevents.Add(mapper.Map<EventResultDTO>(entity));
+        }
+
+        return new Responce<IEnumerable<EventResultDTO>>
+        {
+            StatusCode = 200,
+            Message = "Success",
+            Result = resultevents
+        };
     }
 
-    public Task<Responce<EventResultDTO>> UpdateAsync(EventUpdateDTO dto)
+    public async Task<Responce<EventResultDTO>> GetByIdAsync(long id)
     {
-        throw new NotImplementedException();
+        EventEntity existEvent = await unitOfWork.events.GetByIdAsync(id);
+
+        if (existEvent is null)
+            return new Responce<EventResultDTO>
+            {
+                StatusCode = 404,
+                Message = "This event was not found",
+            };
+
+        var resultEvent = mapper.Map<EventResultDTO>(existEvent);
+
+        return new Responce<EventResultDTO>
+        {
+            StatusCode = 200,
+            Message = "Success",
+            Result = resultEvent
+        };
     }
 }
