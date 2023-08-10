d0052058ca29431a7747ed5ee906e55a9b01b3a2
TMuhammadqodir <muhammadqodirturdiyev65@gmail.com>
2023-08-10 17:46:59 +0500

    AddPropertiesToEventEntity

--- EventPlanningAssistent.Domain/Entities/Events/EventEntity.cs
+++ EventPlanningAssistent.Domain/Entities/Events/EventEntity.cs
@@ -1,5 +1,14 @@
-﻿namespace EventPlanningAssistent.Domain.Entities.Events;
+﻿using EventPlanningAssistent.Domain.Entities.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
 
+namespace EventPlanningAssistent.Domain.Entities.Events;
+
+[Table("Events")]
 public class EventEntity
 {
+    public string Name { get; set; }
+    public DateTime Date { get; set; }
+    public string Location { get; set; }
+    public string Description { get; set; }
+    ICollection<TaskEntity> Tasks { get; set; }
 }
