f7d24fb181b913c0aed36c9d9568642883155a32
TMuhammadqodir <muhammadqodirturdiyev65@gmail.com>
2023-08-10 19:08:41 +0500

    AddMigrations

--- EventPlanningAssistent.Data/DbContexts/AppDbContext.cs
+++ EventPlanningAssistent.Data/DbContexts/AppDbContext.cs
@@ -1,4 +1,10 @@
 ﻿using EventPlanningAssistent.Data.Constants;
+using EventPlanningAssistent.Domain.Entities.Attendees;
+using EventPlanningAssistent.Domain.Entities.Contracts;
+using EventPlanningAssistent.Domain.Entities.Events;
+using EventPlanningAssistent.Domain.Entities.EventVentors;
+using EventPlanningAssistent.Domain.Entities.Tasks;
+using EventPlanningAssistent.Domain.Entities.Ventors;
 using Microsoft.EntityFrameworkCore;
 
 namespace EventPlanningAssistent.Data.DbContexts;
@@ -11,4 +17,11 @@ public class AppDbContext : DbContext
 
         optionsBuilder.UseNpgsql(DatabasePath.EVENT_PLANNING_ASSISTENT_DB_PATH);
     }
+
+    public DbSet<AttendeeEntity> Attendees { get; set; }
+    public DbSet<ContractEntity> Contracts { get; set; }
+    public DbSet<EventEntity> Entits { get; set; }
+    public DbSet<EventVentorEntity> EventVentors { get; set; }
+    public DbSet<TaskEntity> Tasks { get; set; }
+    public DbSet<VentorEntity> Ventors { get; set; }
 }
